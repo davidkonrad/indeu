a928219a08c1cc74db17712f7837b2e9add6f80e
davidkonrad <davidkonrad@gmail.com>
2018-01-07 10:01:08 +0100

    readMore scroll and icon fixes

--- app/directives/readMore.directive.js
+++ app/directives/readMore.directive.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('indeuApp').directive('readMore', function($timeout) {
+angular.module('indeuApp').directive('readMore', function($timeout, $window) {
 
 	return {
 		restrict: 'AE',
@@ -13,26 +13,29 @@ angular.module('indeuApp').directive('readMore', function($timeout) {
 
 					var linkSuffix = new Date().getTime();
 					var readMoreClass = 'readMore'+linkSuffix;
-					var moreText = ' ▼  Læs mere';
-					//var moreText = '<i class="fa fa-chevron-down"></i> Læs mere';
-					var lessText = ' ▲  Læs mindre';
-					//var lessText = '<i class="fa fa-chevron-up"></i> Læs mindre';
-					var link = 	'<h5 class="no-padding"><a class="unstyled text-primary ' + readMoreClass + '">' + moreText + '</a></h5>';
+					//var moreText = ' ▼ Læs mere';
+					var moreText = '<i class="fa fa-chevron-down text-muted"></i> Læs mere';
+					//var lessText = ' ▲  Læs mindre';
+					var lessText = '<i class="fa fa-chevron-up text-muted"></i> Læs mindre';
+					var link = '<h5 class="no-padding"><a class="unstyled text-primary ' + readMoreClass + '">' + moreText + '</a></h5>';
 
 					element[0].style.height = '100px';				
 					element[0].style.overflowY = 'hidden';
 					
 					$(element).after(link);
 					$('body').on('click', '.' + readMoreClass, function() {
-						if ($(this).text() == moreText) {
+						var $this = $(this);
+						if ($this.html() == moreText) {
 							element[0].style.height = 'auto';
 							element[0].style.overflowY = 'visible';
-							$(this).html(lessText);
+							$this.html(lessText);
 						} else {
 							element[0].style.height = '100px';				
 							element[0].style.overflowY = 'hidden';
-							$(this).html(moreText);
-							$("body").animate({scrollTop: $(this).offset().top -100}, "slow");
+							$this.html(moreText);
+							$timeout(function() {
+								$("html, body").animate({ scrollTop: $this.offset().top-120 }, 300);
+							})
 						}
 					});
 				}
