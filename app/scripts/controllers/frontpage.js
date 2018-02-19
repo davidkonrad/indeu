f6ec0f30902ed6a9d15419b342de701baf76d248
davidkonrad <davidkonrad@gmail.com>
2018-02-19 22:59:07 +0100

    restyled frontpage

--- app/scripts/controllers/frontpage.js
+++ app/scripts/controllers/frontpage.js
@@ -20,28 +20,45 @@ angular.module('indeuApp')
 				f.data.forEach(function(c) {
 					switch (c.type) {
 						case 'Artikel':
-							if (c.image) c.image_url = '../media/artikel/'+c.image;
+							if (c.image) c.image_url = '/media/artikel/'+c.image;
 							c.url = Utils.articleUrl(c.content_id, c.header);
 							break;
 
 						case 'Forening':
-							if (c.image) c.image_url = '..media/forening/'+c.image;
+							if (c.image) c.image_url = '../media/forening/'+c.image;
 							c.url = Utils.foreningUrl(c.content_id, c.header);
 							break;
 
 						case 'Event':
-							if (c.image) c.image_url = '..media/artikel/'+c.image;
+							if (c.image) c.image_url = '../media/event/'+c.image;
 							c.url = Utils.eventUrl(c.content_id, c.header);
 							break;
 
 						case 'Statisk':
-							if (c.image) c.image_url = '..media/statisk/'+c.image;
+							if (c.image) c.image_url = 'media/statisk/'+c.image;
 							c.url = Utils.staticUrl(c.content_id, c.header);
 							break;
 
 						default:
 							break; //should really never happen
 					}
+
+					c.image_style = {	'background':'#dadada' };
+					if (c.image_url) {
+						console.log(c.image_url)
+						var img = new Image();
+						img.onload = function() {
+							console.log('onload', c.id, c.image_url);
+							var ret = {
+								'background-image':'url(' + c.image_url + ')',
+								'background-size': 'cover',
+								'background-repeat': 'no-repeat',
+								'background-position': 'center center'
+					    };
+							c.image_style = ret;
+						}
+						img.src = c.image_url;
+					}(c)
 				})
 				$scope.promo = f.data[0];
 				$scope.promo.extract = $scope.promo.extract ? '»'+$scope.promo.extract+'«' : null;
